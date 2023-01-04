4fc3b7531fecf917dcec582697553795d91792ef
Физик <alek7atw@gmail.com>
2023-01-04 23:13:32 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Cmake-Template Repo
+
+This Repo is for those who want to use `The Cherno`'s library but would rather use good old CMake (with maybe VS Code) than the full fat version of Visual Studio that he set up the project to use. 
+
+NOW SUPPORTS GCC (tested on windows only)
+
+I'm looking into Linux compatibility.
+
+# Requirements
+- [CMake](https://cmake.org/)
+- x64 Compiler (DOES NOT WORK WITH x86 COMPILERS!!!) (Currently tested `only` with [Visual Studio 22 Build Tools](https://visualstudio.microsoft.com/downloads/) and [MinGW GCC](https://github.com/niXman/mingw-builds-binaries/releases))
+- [Vulkan SDK](https://vulkan.lunarg.com/sdk/home#windows)
+
+## Profiling
+
+You can use https://ui.perfetto.dev/ to view the profiling data. The data is stored in `profiling/` and is named `perfetto_trace.json`.
---
+++ src/Main.cpp
@@ -0,0 +1,56 @@
+#include "Constants.hpp"
+
+#ifdef _WIN32
+
+#include "Walnut/Application.h"
+#include "Walnut/EntryPoint.h"
+
+#include "Walnut/Image.h"
+
+class ExampleLayer : public Walnut::Layer
+{
+public:
+	virtual void OnUIRender() override // Called every frame
+	{
+		PROFILE_FUNCTION();
+
+		ImGui::Begin("Hello");
+		ImGui::Button("Button");
+		ImGui::End();
+
+		ImGui::ShowDemoWindow();
+	}
+};
+
+Walnut::Application *Walnut::CreateApplication([[maybe_unused]] int argc, [[maybe_unused]] char **argv)
+{
+	Instrumentor::Get().beginSession("Main func"); // Start profiling session
+
+	TimedBlock block("Main func"); // Start timer block
+
+	Walnut::ApplicationSpecification spec; // Create application specification
+	spec.Name = "Walnut Example";		   // Set the name of the application
+
+	Walnut::Application *app = new Walnut::Application(spec); // Create the application
+	app->PushLayer<ExampleLayer>();							  // Push the layer to the application
+	app->SetMenubarCallback([app]()
+							{
+		if (ImGui::BeginMenu("File"))
+		{
+			if (ImGui::MenuItem("Exit"))
+			{
+				app->Close();
+			}
+			ImGui::EndMenu();
+		} });
+	return app;
+}
+
+#else
+
+int main(int, char **)
+{
+	print("GNU/Linux is not yet supported (but i'm looking into it).\n");
+}
+
+#endif
